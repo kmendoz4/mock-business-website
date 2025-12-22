8c0fccbf87a19025584294db377e4a4aa63ac73a
Kayla Mendoza <mendozakayla00@gmail.com>
2025-12-21 20:52:17 -0500

    Second commit - working on hero section

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('#mobile-menu')
+const menu = document.querySelector('#mobile-menu') // selects the menu icon by its id
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function() {
